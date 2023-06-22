04e831f1c4509c31e793f4c38f34d4a2f26e605a
Kareem Skinner <kareem@karsteski.com>
2023-06-21 21:16:55 -0400

    Added a script that generates python ui_files

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 #!/bin/python
 
+from PySide6.QtWidgets import QApplication
+
+from PySide6.QtCore import QFile, QIODeviceBase
+from PySide6.QtWidgets import QMainWindow
+from PySide6.QtUiTools import QUiLoader
+
 from typing import Any
 import sys
 
-from PySide6.QtWidgets import QApplication
-from PySide6.QtQuick import QQuickView
-
-
-
 import file_data
 import api_data
+import ui.main_window
+
 from items import *
 
 FILENAME = "../data/ao-bin-dumps/formatted/items.txt"
@@ -28,15 +31,24 @@ for thingy in found_thingies:
     items.append([item for item in item_data if item.name == thingy][0])
 
 # response : list[dict[str, Any]]= api_data.getCurrentPrices(items,
-#                                 [api_data.City.Lymhurst],
-#                                 )
+                                # [api_data.City.Lymhurst],
+                                # )
 # lol :list[MarketData]= api_data.marketResponsetoMarketData(response)
-#
 
-app = QApplication()
-view = QQuickView()
 
-view.setSource("qml/view.qml")
-view.show()
-sys.exit(app.exec())
+application = QApplication([])
 
+
+qUiLoader = QUiLoader()
+
+path = ""
+file = QFile("/home/karsteski/dev/albionameliore/src/ui/ui_files/albionameliore.ui")
+file.open(QIODeviceBase.OpenModeFlag.ReadOnly)
+
+albion_ameliore = qUiLoader.load(file, None)
+albion_ameliore.show()
+
+# main_window = ui.main_window.MainWindow()
+# main_window.show()
+
+application.exec()
